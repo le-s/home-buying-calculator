58b20e6ac5326fa5df03cbab2cbdcdea774c2f99
Steven Le <steven.an.le@gmail.com>
2019-04-25 13:56:50 -0700

    Create Fiscal Hive project

---
+++ src/components/navbar/nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let nav = (
+      <div>
+        <h1>hello</h1>
+      </div>
+    )
+
+    return (
+      <div>
+        {nav}
+      </div>
+    )
+  }
+}
+
+export default NavBar;
+
